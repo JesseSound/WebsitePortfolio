d825ff030f87b9ecc27cd6e5588473df116822e6
200570287 <jesseearlemccormack@gmail.com>
2025-02-26 15:43:05 -0500

    Slight update

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[name][ext][query]', // To specify how to name and place assets
   },
   module: {
     rules: [
@@ -14,6 +15,13 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(glb|gltf)$/i, // Match .glb and .gltf files
+        type: 'asset/resource', // Treat these files as static assets
+        generator: {
+          filename: 'Models/[name][ext][query]', // Place them in the Models folder
         },
       },
     ],
@@ -30,6 +38,6 @@ module.exports = {
   },
   mode: 'production', // Change to 'development' for dev mode
   stats: {
-    children: true, // Removed the semicolon here, it should be a comma.
+    children: true, // Enable stats for child compilations
   },
 };

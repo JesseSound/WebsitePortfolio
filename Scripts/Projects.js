5e058f8a72a401274fd5374343e350fce243fb3c
200570287 <jesseearlemccormack@gmail.com>
2025-04-06 23:03:42 -0400

    bring back my cubes

--- Scripts/Projects.js
+++ Scripts/Projects.js
@@ -1,6 +1,6 @@
 // Initial scene setup
 let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(1000, window.innerWidth / window.innerHeight, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(200, window.innerWidth / window.innerHeight, 0.1, 1000);
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);

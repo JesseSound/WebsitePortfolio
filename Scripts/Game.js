c4c656c53320d26dd5df0f1b8bbd32c3c767ee0a
200570287 <jesseearlemccormack@gmail.com>
2025-04-17 07:18:26 -0400

    More fucking cubes

--- Scripts/Game.js
+++ Scripts/Game.js
@@ -212,7 +212,7 @@ const maxY = screenHeight / 2 - 50; // Adjust based on spaceship size
 
 function animate() {
     requestAnimationFrame(animate);
-    if (Math.random() < 0.5 && cubes.length < 100) {
+    if (Math.random() < 0.5 && cubes.length < 1300) {
         createCube();
     }
 
